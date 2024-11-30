6ca33a6058daa89341fdb34d48961954588c3558
agatoneq <agata.sobczyk444@wp.pl>
2024-11-30 13:43:48 +0100

    Base

---
+++ SelfSpellBackend/run.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import jsonify
+from flask.cli import load_dotenv
+from app.app import create_app
+
+load_dotenv()
+
+config=os.getenv('FLASK_ENV') or 'development'
+
+app = create_app(config)
+
+@app.route("/api/users", methods=["GET"])
+def users():
+    return jsonify(
+        {
+            "users":[
+                'user1',
+                'user2'
+                ]
+        }
+    )
+        
+if __name__ == "__main__":
+    if config == 'development':
+        app.run(debug=True)
+    else:
+        from werkzeug.serving import run_simple
+        run_simple('0.0.0.0', 5000, app)
